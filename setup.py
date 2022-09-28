5d3d9c60dfc6d622c309faeecd9baf23486f5f2c
Paulo Fappi <paulo.fappi@jumplabel.com.br>
2022-09-28 12:10:59 -0300

    Correcao pacote

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='teste',
     version='0.0.1',
     author='Paulo Fappi',
     author_email='paulo.fappi@jumplabel.com.br',
     description='Testing installation of Package',
-    long_description=long_description,
+    long_description="Lorem ipsum teste",
     long_description_content_type="text/markdown",
     url='https://bitbucket.org/jumplabel-projetos/teste-packege/',
     project_urls = {
         "Bug Tracker": "https://github.com/Muls/toolbox/issues"
     },
     license='MIT',
-    packages=['teste-packege'],
+    packages=['teste'],
     install_requires=['requests'],
 )
